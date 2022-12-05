c079cc1a577e308cf3b23ed244eb5269e87c25c8
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-12-05 13:45:08 +0100

    implementing productService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,11 +7,20 @@ import { Product } from '../classes/product';
   providedIn: 'root'
 })
 export class ProductService {
-  apiUrl='http://localhost:8080/products';
-  
+  apiUrl='http://localhost:8080/products';//the backend api url
+
   constructor(private http:HttpClient) { }//declare an instance of http client
-  getProducts():Observable<any>{
-    return this.http.get(this.apiUrl+'/all');
-    
+ //get all products
+  getProducts():Observable<Product[]>{
+    return this.http.get<Product[]>(this.apiUrl+'/all');
   }
+  //get product by id
+  getProduct(id:number):Observable<Product>{
+    return this.http.get<Product>(this.apiUrl+'/product/'+id);
+  }
+  //add product
+  addProduct(product:Product):Observable<Product>{
+    return this.http.post<Product>(this.apiUrl+'/add',product);
+  }
+
 }
